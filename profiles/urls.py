746a35f46d34d72cb7479cc52ccb46d1b183b677
Andy G <andyguttridge123@gmail.com>
2022-12-01 15:17:34 +0000

    Feature: add Post model and serializer

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
     # This is the url for our detail view which allows us to retrieve
     # a single profile by its primary key in the URL.
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view())
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
