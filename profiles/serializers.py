58d316bb0d739335a66b16d0d6b6b6df343cac07
Andy G <andyguttridge123@gmail.com>
2022-12-01 09:51:18 +0000

    Feature: add serializer, view and url for profile model

---
+++ profiles/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    # Make the owner field read only
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Profile
+        # Here we specify the fields we want to include in the serialized data.
+        # Note the id field is created automatically on our model by Django,
+        # but we still have to specify it if we want to include it in the
+        # serialized data.
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image'
+        ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        # Create a serializer instance. many=True specifies that we are
+        # serializing multiple profile instances.
+        serializer = ProfileSerializer(profiles, many=True)
+        # Instead of returning objects like we might in a template based Django
+        # app, we return the data from our serializer.
+        return Response(serializer.data)
