266aa8f48ee46414a35263795427eaa036fa142b
Andy G <andyguttridge123@gmail.com>
2022-12-02 12:14:02 +0000

    Feature: add LikesList and LikesDetail views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+# We subclass ListCreateAPIView so that we get our
+# GET and POST methods for free.
+class LikeList(generics.ListCreateAPIView):
+    # Ensure only authenticated users can create a like
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    # Use the perform_create method to set the owner of the like
+    # to the user making the request.
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+# We sub-class the RetrieveDestroyAPI view, as we only want to retrieve
+# and delete likes. There's no need to update.
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    # Ensure only authenticated users can create a like
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
